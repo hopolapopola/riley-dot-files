99832f2c5ccabf6a7921ce45a526853f39d7d958
Riley <timemachine1x1@gmail.com>
2020-11-03 15:56:56 +1100

    add the files

---
+++ install_zsh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Install zsh (debian/ubuntu)
+sudo apt install zsh
+#Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#Grab the extension I like
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#Put the custom .zshrc into the home folder (IT ONLY WORKS FOR me, i.e. username=riley)
+cp .zshrc ~/.zshrc 
