44f98a08fd66f19b4ec9bad060aef30313096614
Riley <timemachine1x1@gmail.com>
2021-02-05 21:56:21 +1100

    fix a lot of things (hopefully)

--- install_zsh.sh
+++ install_zsh.sh
@@ -9,8 +9,9 @@ elif [ "$dist" = "Ubun" ] || [ "$dist" = "Debi" ]; then
 	cp .zshrc_buntu ~/.zshrc
 fi
 #Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+chsh -s $(which zsh)
 #Grab the extension I like
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#Put the custom .zshrc into the home folder and the theme too :)
+#Put the custom theme into the right folder :)
 cp candy_custom.zsh-theme ~/.oh-my-zsh/themes
