53c4a376ab6e64f26f11a1c8ff0a5aaa66f881fd
Riley <timemachine1x1@gmail.com>
2021-02-05 21:49:59 +1100

    realise that ftc is arch only and fix that bit of the .zshrc

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Test for distro
+# Install zsh and move custom zshrc
 dist=$(cat /etc/issue | cut -c-4)
 if [ "$dist" = "Arch" ]; then
 	sudo pacman -Syu zsh
+	cp .zshrc ~/.zshrc
 elif [ "$dist" = "Ubun" ] || [ "$dist" = "Debi" ]; then
 	sudo apt install zsh
+	cp .zshrc_buntu ~/.zshrc
 fi
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #Grab the extension I like
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#Put the custom .zshrc into the home folder :)
-cp .zshrc ~/.zshrc 
+#Put the custom .zshrc into the home folder and the theme too :)
+cp candy_custom.zsh-theme ~/.oh-my-zsh/themes
