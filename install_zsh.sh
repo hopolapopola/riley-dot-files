a8e07fb3a086a54cf56dab95588905c802361c64
Riley <timemachine1x1@gmail.com>
2021-02-05 21:17:26 +1100

    make install script better; fix home dir

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#Install zsh (debian/ubuntu)
-# sudo apt install zsh
+# Test for distro
+dist=$(cat /etc/issue | cut -c-4)
+if [ "$dist" = "Arch"]; then
+	sudo pacman -Syu zsh
+elif [ "$dist" = "Ubun" || "$dist" = "Debi" ]; then
+	sudo apt install zsh
+fi
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #Grab the extension I like
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#Put the custom .zshrc into the home folder (IT ONLY WORKS FOR me, i.e. username=riley)
+#Put the custom .zshrc into the home folder :)
 cp .zshrc ~/.zshrc 
